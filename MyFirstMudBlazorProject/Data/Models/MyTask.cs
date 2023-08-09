dcc15b647184e2bb3c4fdbfb32612eb07971d0e4
DankVader17 <arabandproud17@gmail.com>
2023-08-09 09:44:02 -0700

    Update: Inputs - can leave description blank - due date is set to today, by default - priority now has a default "N/A"
    * What's left:
    - handle empty title exception
    -Bonus: unit and integration testing

--- MyFirstMudBlazorProject/Data/Models/MyTask.cs
+++ MyFirstMudBlazorProject/Data/Models/MyTask.cs
@@ -6,10 +6,11 @@ namespace MyFirstMudBlazorProject.Data.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime? DueDate { get; set; }
-        public string Priority { get; set; }
+        public string Description { get; set; } = "";
+        public DateTime? DueDate { get; set; } = DateTime.Today;
+        public string Priority { get; set; } = "N/A";
         public bool IsComplete { get; set; }
     }
 }
